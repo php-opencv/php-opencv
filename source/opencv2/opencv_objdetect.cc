841276a8fb088c57d02f4593f22f939b267cda06
Vladimir Goncharov <morozovsk@gmail.com>
2023-02-03 04:30:37 +0200

    backporting for windows support and FaceDetectorYN

--- source/opencv2/opencv_objdetect.cc
+++ source/opencv2/opencv_objdetect.cc
@@ -21,6 +21,14 @@
 #include "core/opencv_mat.h"
 
 zend_class_entry *opencv_cascade_classifier_ce;
+zend_class_entry *opencv_facedetectoryn_ce;
+
+/**
+ * -----------------------------------【CV\Cascadeclassifier】--------------------------
+ *
+ * -------------------------------------------------------------------------------------
+ */
+
 
 zend_object_handlers opencv_cascade_classifier_object_handlers;
 
@@ -174,6 +182,256 @@ void opencv_cascade_classifier_init(int module_number){
 }
 
 
+/**
+ * -----------------------------------【CV\FaceDetectorYN】-----------------------------
+ *
+ * -------------------------------------------------------------------------------------
+ */
+
+#ifdef OPENCV_OBJDETECT_FACE_HPP
+
+zend_object_handlers opencv_facedetectoryn_object_handlers;
+
+
+PHP_METHOD(opencv_facedetectoryn, create)
+{
+    char *model;
+    size_t model_length;
+    char *config;
+    size_t config_length;
+    zval *size_zval;
+    double score_threshold = 0.9;
+    double nms_threshold = 0.3;
+    long top_k = 5000;
+    long backend_id = 0;
+    long target_id = 0;
+
+    Size size = Size();
+
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "ssO|ddlll",
+                              &model, &model_length,
+                              &config, &config_length,
+                              &size_zval, opencv_size_ce,
+                              &score_threshold, &nms_threshold, &top_k, &backend_id, &target_id) == FAILURE) {
+        RETURN_NULL();
+    }
+
+    zval instance;
+    object_init_ex(&instance, opencv_facedetectoryn_ce);
+    opencv_facedetectoryn_object *facedetectoryn_obj = Z_PHP_FACEDETECTORYN_OBJ_P(&instance);
+
+    opencv_size_object *size_object = Z_PHP_SIZE_OBJ_P(size_zval);
+    size = *size_object->size;
+
+    facedetectoryn_obj->facedetectoryn = FaceDetectorYN::create(model, config, size, score_threshold, nms_threshold, top_k, backend_id, target_id);
+
+    RETURN_ZVAL(&instance,0,0); //return php FaceDetectorYN object
+}
+
+PHP_METHOD(opencv_facedetectoryn, detect)
+{
+    zval *image_zval;
+    Mat faces;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "O",
+                              &image_zval, opencv_mat_ce) == FAILURE) {
+        RETURN_NULL();
+    }
+
+    opencv_mat_object *image_obj = Z_PHP_MAT_OBJ_P(image_zval);
+
+    opencv_facedetectoryn_object *obj = Z_PHP_FACEDETECTORYN_OBJ_P(getThis());
+    obj->facedetectoryn->detect(*image_obj->mat, faces);
+
+
+    zval faces_zval;
+    object_init_ex(&faces_zval, opencv_mat_ce);
+    opencv_mat_object *faces_obj = Z_PHP_MAT_OBJ_P(&faces_zval);
+    faces_obj->mat = new Mat(faces);
+
+    opencv_mat_update_property_by_c_mat(&faces_zval, faces_obj->mat);
+
+    RETURN_ZVAL(&faces_zval,0,0); //return php Mat object
+}
+
+PHP_METHOD(opencv_facedetectoryn, setInputSize)
+{
+    zval *size_zval;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "O",
+                              &size_zval, opencv_size_ce) == FAILURE) {
+        RETURN_NULL();
+    }
+
+    opencv_size_object *size_object = Z_PHP_SIZE_OBJ_P(size_zval);
+    Size size = *size_object->size;
+
+    opencv_facedetectoryn_object *obj = Z_PHP_FACEDETECTORYN_OBJ_P(getThis());
+    obj->facedetectoryn->setInputSize(size);
+
+    RETURN_NULL();
+}
+
+PHP_METHOD(opencv_facedetectoryn, setNMSThreshold)
+{
+    double nms_threshold;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "d",
+                              &nms_threshold) == FAILURE) {
+        RETURN_NULL();
+    }
+
+    opencv_facedetectoryn_object *obj = Z_PHP_FACEDETECTORYN_OBJ_P(getThis());
+    obj->facedetectoryn->setNMSThreshold(nms_threshold);
+
+    RETURN_NULL();
+}
+
+PHP_METHOD(opencv_facedetectoryn, setScoreThreshold)
+{
+    double score_threshold;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "d",
+                              &score_threshold) == FAILURE) {
+        RETURN_NULL();
+    }
+
+    opencv_facedetectoryn_object *obj = Z_PHP_FACEDETECTORYN_OBJ_P(getThis());
+    obj->facedetectoryn->setScoreThreshold(score_threshold);
+
+    RETURN_NULL();
+}
+
+PHP_METHOD(opencv_facedetectoryn, setTopK)
+{
+    long top_k;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "l",
+                              &top_k) == FAILURE) {
+        RETURN_NULL();
+    }
+
+    opencv_facedetectoryn_object *obj = Z_PHP_FACEDETECTORYN_OBJ_P(getThis());
+    obj->facedetectoryn->setTopK(top_k);
+
+    RETURN_NULL();
+}
+
+PHP_METHOD(opencv_facedetectoryn, getInputSize)
+{
+    zval *size_zval;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "") == FAILURE) {
+        RETURN_NULL();
+    }
+
+    opencv_facedetectoryn_object *obj = Z_PHP_FACEDETECTORYN_OBJ_P(getThis());
+    Size size = obj->facedetectoryn->getInputSize();
+
+    zval instance;
+    object_init_ex(&instance, opencv_size_ce);
+    opencv_size_object *size_object = Z_PHP_SIZE_OBJ_P(&instance);
+    size_object->size = new Size(size);
+
+    opencv_size_update_property_by_c_size(&instance, size_object->size);
+    RETURN_ZVAL(&instance,0,0);
+}
+
+PHP_METHOD(opencv_facedetectoryn, getNMSThreshold)
+{
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "") == FAILURE) {
+        RETURN_NULL();
+    }
+
+    opencv_facedetectoryn_object *obj = Z_PHP_FACEDETECTORYN_OBJ_P(getThis());
+    double nms_threshold = obj->facedetectoryn->getNMSThreshold();
+
+    RETURN_DOUBLE(nms_threshold);
+}
+
+PHP_METHOD(opencv_facedetectoryn, getScoreThreshold)
+{
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "") == FAILURE) {
+        RETURN_NULL();
+    }
+
+    opencv_facedetectoryn_object *obj = Z_PHP_FACEDETECTORYN_OBJ_P(getThis());
+    double score_threshold = obj->facedetectoryn->getScoreThreshold();
+
+    RETURN_DOUBLE(score_threshold);
+}
+
+PHP_METHOD(opencv_facedetectoryn, getTopK)
+{
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "") == FAILURE) {
+        RETURN_NULL();
+    }
+
+    opencv_facedetectoryn_object *obj = Z_PHP_FACEDETECTORYN_OBJ_P(getThis());
+    long top_k = obj->facedetectoryn->getTopK();
+
+    RETURN_LONG(top_k);
+}
+
+/**
+ * opencv_facedetectoryn_methods[]
+ */
+const zend_function_entry opencv_facedetectoryn_methods[] = {
+        PHP_ME(opencv_facedetectoryn, create, arginfo_void, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+        PHP_ME(opencv_facedetectoryn, detect, arginfo_void, ZEND_ACC_PUBLIC)
+        PHP_ME(opencv_facedetectoryn, getInputSize, arginfo_void, ZEND_ACC_PUBLIC)
+        PHP_ME(opencv_facedetectoryn, getNMSThreshold, arginfo_void, ZEND_ACC_PUBLIC)
+        PHP_ME(opencv_facedetectoryn, getScoreThreshold, arginfo_void, ZEND_ACC_PUBLIC)
+        PHP_ME(opencv_facedetectoryn, getTopK, arginfo_void, ZEND_ACC_PUBLIC)
+        PHP_ME(opencv_facedetectoryn, setInputSize, arginfo_void, ZEND_ACC_PUBLIC)
+        PHP_ME(opencv_facedetectoryn, setNMSThreshold, arginfo_void, ZEND_ACC_PUBLIC)
+        PHP_ME(opencv_facedetectoryn, setScoreThreshold, arginfo_void, ZEND_ACC_PUBLIC)
+        PHP_ME(opencv_facedetectoryn, setTopK, arginfo_void, ZEND_ACC_PUBLIC)
+        PHP_FE_END
+};
+/* }}} */
+
+zend_object* opencv_facedetectoryn_create_handler(zend_class_entry *type)
+{
+    size_t size = sizeof(opencv_facedetectoryn_object)+zend_object_properties_size(type);
+    opencv_facedetectoryn_object *obj = (opencv_facedetectoryn_object *)ecalloc(1, size);
+    memset(obj, 0, size);
+    zend_object_std_init(&obj->std, type);
+    object_properties_init(&obj->std, type);
+    obj->std.ce = type;
+    obj->std.handlers = &opencv_facedetectoryn_object_handlers;
+    return &obj->std;
+}
+
+void opencv_facedetectoryn_free_obj(zend_object *object)
+{
+    opencv_facedetectoryn_object *obj;
+    obj = get_facedetectoryn_obj(object);
+    zend_object_std_dtor(object);
+}
+
+void opencv_facedetectoryn_init(int module_number)
+{
+    zend_class_entry ce;
+    INIT_NS_CLASS_ENTRY(ce, OPENCV_NS, "FaceDetectorYN", opencv_facedetectoryn_methods);
+    opencv_facedetectoryn_ce = zend_register_internal_class(&ce);
+
+    opencv_facedetectoryn_ce->create_object = opencv_facedetectoryn_create_handler;
+    memcpy(&opencv_facedetectoryn_object_handlers,
+           zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+    opencv_facedetectoryn_object_handlers.clone_obj = NULL;
+    opencv_facedetectoryn_object_handlers.free_obj = opencv_facedetectoryn_free_obj;
+    opencv_facedetectoryn_object_handlers.offset = XtOffsetOf(opencv_facedetectoryn_object, std);
+}
+
+
+#endif
+
+/**
+ * -------------------------------
+ */
+
 
 void opencv_objdetect_constants_init(int module_number)
 {
@@ -189,7 +447,14 @@ void opencv_objdetect_constants_init(int module_number)
     REGISTER_NS_LONG_CONSTANT(OPENCV_NS, "CASCADE_DO_ROUGH_SEARCH", CASCADE_DO_ROUGH_SEARCH, CONST_CS | CONST_PERSISTENT);
 }
 
+/**
+ * Objdetect Init
+ */
 void opencv_objdetect_init(int module_number){
     opencv_cascade_classifier_init(module_number);
     opencv_objdetect_constants_init(module_number);
+
+#ifdef OPENCV_OBJDETECT_FACE_HPP
+    opencv_facedetectoryn_init(module_number);
+#endif
 }
