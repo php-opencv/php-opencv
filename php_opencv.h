957a4fe19404f7ec3823e8024857225e4dece384
BbenWeb1 <124698488+BbenWeb1@users.noreply.github.com>
2023-06-16 10:41:47 +0200

    Update php_opencv.h

--- php_opencv.h
+++ php_opencv.h
@@ -52,7 +52,7 @@ using namespace cv;
 #define OPENCV_ML_NS ZEND_NS_NAME(OPENCV_NS,"ML")
 #define OPENCV_DNN_NS ZEND_NS_NAME(OPENCV_NS,"DNN")
 #define OPENCV_DNN_SUPERRES_NS ZEND_NS_NAME(OPENCV_NS,"dnn_superres")
-#define OPENCV_TEXT_OCR_NS ZEND_NS_NAME(OPENCV_NS,'text')
+#define OPENCV_TEXT_OCR_NS ZEND_NS_NAME(OPENCV_NS,"text")
 #define OPENCV_CONNECT(text1,text2) text1##text2
 
 
